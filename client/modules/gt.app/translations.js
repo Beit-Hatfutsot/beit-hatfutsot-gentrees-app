46f7e3c8d192656d57849e3dd8f0bef62ce079b4
Dani Kenan <danikenan@gmail.com>
2014-09-09 06:32:37 +0300

    add ux screen shots

--- client/modules/gt.app/translations.js
+++ client/modules/gt.app/translations.js
@@ -23,7 +23,7 @@ angular.module('gt.app').config(['$translateProvider',
             'Enter Value' : 'הכנס ערך',
             'Email' : 'דואר אלקטרוני',
             'AGREE_TO_USAGE_TERMS' : 'קראתי ואני מסכים ל<a href="usageterms.html">תנאי השימוש</a>',
-            'I have read and I agree to the <a href="{0}">usage terms</a>': 'קראתי ואני מסכים לתנאי השימוש',
+            //'I have read and I agree to the <a href="{0}">usage terms</a>': 'קראתי ואני מסכים לתנאי השימוש',
             'In this site you can create your family tree in 4 simple steps' : 'באתר זה תוכלו ליצור את עץ המשפחה שלכם ב-4 צעדים פשוטים',
             'The family tree will be saved for you in the Beit Hatfutsot archive and you will be able to watch and edit it in the future in Beit Hatfutsot archive site' : 'העץ ישמר בארכיון בית התפוצות ותוכלו לעיין בו ולערוך אותו בעתיד באתר המאגרים של בית התפוצות',
             'Registration Details' : 'פרטי רישום'

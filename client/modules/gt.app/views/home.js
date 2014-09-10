24c6486415400b946f6d73c6ab8534b956322545
yairov <yairov@gmail.com>
2014-09-10 13:07:56 +0300

    scroll to top between steps

--- client/modules/gt.app/views/home.js
+++ client/modules/gt.app/views/home.js
@@ -1,84 +1,94 @@
 'use strict';
 
-angular.module('gt.app').controller('gtHomeCtrl', ['$scope', '$filter', 'gtRegistrationSvc', '$state', '$timeout', function ($scope, $filter, regSvc, $state, $timeout) {
+angular.module('gt.app').controller('gtHomeCtrl', [
+    '$scope', '$filter', 'gtRegistrationSvc', '$state', '$timeout', '$location', '$anchorScroll', function ($scope, $filter, regSvc, $state, $timeout, $location, $anchorScroll) {
 
-    $scope.loading = false;
+        var scrollToTop = function () {
+            $location.hash('top');
+            $anchorScroll();
+        };
 
-    $scope.error = undefined;
+        $scope.loading = false;
 
-    $scope.info = undefined;
-
-    $scope.registrationCode = undefined;
-
-    $scope.step = 1;
-
-    $scope.stepCount = 5;
-
-    $scope.next = function () {
-        $scope.step++;
-    };
-
-    $scope.back = function () {
-        $scope.step--;
-    };
-
-    $scope.isRegistrationConfirmed = function () {
-        return regSvc.confirmed;
-    };
-
-    $scope.resendRegistrationMail = function () {
-        regSvc.sendMail($scope.model.email);
-    };
-
-    $scope.confirmRegistration = function () {
-        regSvc.confirm($scope.registrationCode);
-    };
-
-    $scope.save = function () {
-        $scope.info = undefined;
         $scope.error = undefined;
-        $scope.loading = true;
 
-        return regSvc.saveModel().then(
-            function () {
-                $scope.info = 'Your data has been successfully saved.';
-                $timeout(function(){
-                    $state.go('welcome');
-                }, 3500);
-            },
-            function (error) {
-                $scope.error = error;
-            })
-            .finally(function () {
-                $scope.loading = false;
-            });
-    };
+        $scope.info = undefined;
 
-    $scope.$watch('model.numBrothers', function (newValue, oldValue) {
-        newValue = parseInt($filter('number')(newValue)) || 0;
-        oldValue = oldValue || 0;
+        $scope.registrationCode = undefined;
 
-        if (newValue === oldValue) {
-            return;
-        }
+        $scope.step = 1;
 
-        if (!newValue || newValue === 0) {
-            $scope.model.brothers = [];
-            return;
-        }
+        $scope.stepCount = 5;
 
-        if (newValue > 0) {
-            if (newValue > oldValue) {
-                for (var x = $scope.model.brothers.length; x < newValue; x++) {
-                    $scope.model.brothers.push({});
+        $scope.next = function () {
+            $scope.step++;
+            scrollToTop();
+        };
+
+        $scope.back = function () {
+            $scope.step--;
+            scrollToTop();
+        };
+
+        $scope.isRegistrationConfirmed = function () {
+            return regSvc.confirmed;
+        };
+
+        $scope.resendRegistrationMail = function () {
+            regSvc.sendMail($scope.model.email);
+        };
+
+        $scope.confirmRegistration = function () {
+            regSvc.confirm($scope.registrationCode);
+        };
+
+        $scope.save = function () {
+            $scope.loading = true;
+
+            return regSvc.saveModel().then(
+                function () {
+                    $scope.info = 'Your data has been successfully saved.';
+                    $timeout(function () {
+                        $state.go('welcome');
+                    }, 3500);
+                },
+                function (error) {
+                    $scope.error = error;
+                })
+                .finally(function () {
+                    $scope.loading = false;
+                    $timeout(function () {
+                        $scope.info = undefined;
+                        $scope.error = undefined;
+                    }, 3500);
+                });
+        };
+
+        $scope.$watch('model.numBrothers', function (newValue, oldValue) {
+            newValue = parseInt($filter('number')(newValue)) || 0;
+            oldValue = oldValue || 0;
+
+            if (newValue === oldValue) {
+                return;
+            }
+
+            if (!newValue || newValue === 0) {
+                $scope.model.brothers = [];
+                return;
+            }
+
+            if (newValue > 0) {
+                if (newValue > oldValue) {
+                    for (var x = $scope.model.brothers.length; x < newValue; x++) {
+                        $scope.model.brothers.push({});
+                    }
+                }
+                else {
+                    for (var y = $scope.model.brothers.length; y > newValue; y--) {
+                        $scope.model.brothers.pop();
+                    }
                 }
             }
-            else {
-                for (var y = $scope.model.brothers.length; y > newValue; y--) {
-                    $scope.model.brothers.pop();
-                }
-            }
-        }
-    });
+        });
 
-}]);
+    }]);

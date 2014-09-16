5f216ff92ee6ef99ad48de504b2df41fbf1d1bf8
Dani Kenan <danikenan@gmail.com>
2014-09-16 12:05:47 +0300

    fixed email not sending + some gitignores

--- client/modules/gt.app/views/register.js
+++ client/modules/gt.app/views/register.js
@@ -4,7 +4,7 @@ angular.module('gt.app').controller('gtRegisterCtrl',
     ['$scope', '$state', 'gtRegistrationSvc', function ($scope, $state, regSvc) {
 
         $scope.register = function(){
-            regSvc.sendMail($scope.model.email).then(function(){
+            regSvc.sendMail($scope.model.me.email).then(function(){
                 $state.go('confirm');
             });
         };

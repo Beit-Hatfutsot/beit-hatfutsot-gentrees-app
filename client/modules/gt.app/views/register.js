241880304e931ec6bab77ddf3da3b156711089d9
asafd <asafdavidi1@gmail.com>
2014-10-06 21:56:55 +0300

    - Home page bg full screen - Home screen button snap to bottom - Spinner on reg

--- client/modules/gt.app/views/register.js
+++ client/modules/gt.app/views/register.js
@@ -4,9 +4,12 @@ angular.module('gt.app').controller('gtRegisterCtrl',
     ['$scope', '$state', 'gtRegistrationSvc', function ($scope, $state, regSvc) {
 
         $scope.register = function(){
+
             regSvc.sendMail($scope.model.me.email).then(function(){
+                $scope.showSpinner=false;
                 $state.go('confirm');
             });
+            $scope.showSpinner=true;
         };
 
     }]);

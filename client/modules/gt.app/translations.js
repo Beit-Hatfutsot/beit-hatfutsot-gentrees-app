681d620af0788ce10764a5e9f62c1dcff907a707
asafd <asafdavidi1@gmail.com>
2014-10-21 12:58:44 +0300

    Translation - update text Person - Changed text placeholders Confirm - min/max length of code is 4

--- client/modules/gt.app/translations.js
+++ client/modules/gt.app/translations.js
@@ -33,8 +33,8 @@ angular.module('gt.app').config(['$translateProvider',
             'Continue': 'המשך',
             'Confirm Email': 'אימות דואר אלקטרוני',
             'AGREE_TO_USAGE_TERMS' : 'קראתי ואני מסכים/ה ל<a href="usageterms.html">תנאי השימוש</a>',
-            'In this site you can create your family tree in 4 simple steps' : 'באתר זה תוכלו ליצור את עץ המשפחה שלכם ב-4 צעדים פשוטים',
-            'The family tree will be saved for you in the Beit Hatfutsot archive and you will be able to watch and edit it in the future in Beit Hatfutsot archive site' : 'העץ ישמר בארכיון בית התפוצות ותוכלו לעיין בו ולערוך אותו בעתיד באתר המאגרים של בית התפוצות',
+            'In this site you can create your family tree in 4 simple steps' : 'ברוכים הבאים לפרויקט עצי המשפחה של צה"ל ובית התפוצות. כאן תוכלו ליצור את עץ המשפחה שלכם ב-4 צעדים פשוטים.',
+            'The family tree will be saved for you in the Beit Hatfutsot archive and you will be able to watch and edit it in the future in Beit Hatfutsot archive site' : 'העץ שתבנו יישמר במאגר עצי המשפחה המאובטח של בית התפוצות ותוכלו לעיין בו ולערוך אותו בעתיד באתר בית התפוצות.',
             'Registration Details' : 'פרטי רישום',
             'How many brothers you have ?': 'כמה אחים יש לך?',
             'Steps': 'שלב {{step}} מתוך {{stepCount}}',
@@ -43,7 +43,10 @@ angular.module('gt.app').config(['$translateProvider',
             'Your data has been successfully saved.': 'הנתונים ששלחת נשמרו בהצלחה.',
             'Confirm Email Description': 'בדקו את תיבת הדואר. בדקות הקרובות תתקבל הודעה ובה קוד אימות, יש להעתיק את הקוד לכאן.',
             'Confirm Email No Mail': 'במידה ולא התקבלה הודעה, בדקו את תיבת דואר הזבל',
-            'Resend':'שלח שוב'
+            'Resend':'שלח שוב',
+            'Click to add':'לחצו להוספה',
+            'Name before marriage':'שם משפחה לפני החתונה',
+            'City and or country':'עיר ו/או מדינה'
         });
 
         $translateProvider.preferredLanguage('he');

993ef5577e8db637a176d69b97d4431774983751
ofermusaipd <ofermusaipd@gmail.com>
2015-07-21 12:03:16 +0300

    script fix

--- client/modules/gt.app/views/home.js
+++ client/modules/gt.app/views/home.js
@@ -93,7 +93,6 @@ angular.module('gt.app').controller('gtHomeCtrl', [
                                     validFiled++;
                                 }
                             });
-
                         }
 
                     } else {

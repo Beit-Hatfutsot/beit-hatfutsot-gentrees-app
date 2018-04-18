740aabef6337f66bbf513dba220f4cde92784145
Ori Hoch <ori@uumpa.com>
2018-04-18 19:28:00 +0300

    .travis

--- .travis.sh
+++ .travis.sh
@@ -18,16 +18,16 @@ elif [ "${1}" == "deploy" ]; then
         [ "$?" != "0" ] && echo failed docker push && exit 1
     else
         docker run -e CLONE_PARAMS="--branch ${K8S_REPO_BRANCH} https://github.com/${K8S_REPO_SLUG}.git" \
-                   -e YAML_UPDATE_TYPE='docker-image-suffixes' \
-                   -e DOCKER_IMAGE_SUFFIXES=${IMAGE_SUFFIXES} \
-                   -e DOCKER_IMAGE_PREFIX=${IMAGE_PREFIX} \
-                   -e DOCKER_IMAGE_UPDATE_MAP_JSON=${UPDATE_MAP_JSON} \
-                   -e DOCKER_IMAGE_TAG=${tag} \
-                   -e YAML_UPDATE_FILE=${YAML_UPDATE_FILE} \
-                   -e GIT_USER_EMAIL=${GIT_USER_EMAIL} \
-                   -e GIT_USER_NAME=${GIT_USER_NAME} \
-                   -e GIT_COMMIT_MESSAGE=${GIT_COMMIT_MESSAGE} \
-                   -e PUSH_PARAMS='https://${GITHUB_TOKEN}@${K8S_REPO_SLUG} ${K8S_REPO_BRANCH}' \
+                   -e YAML_UPDATE_TYPE="docker-image-suffixes" \
+                   -e DOCKER_IMAGE_SUFFIXES="${IMAGE_SUFFIXES}" \
+                   -e DOCKER_IMAGE_PREFIX="${IMAGE_PREFIX}" \
+                   -e DOCKER_IMAGE_UPDATE_MAP_JSON="${UPDATE_MAP_JSON}" \
+                   -e DOCKER_IMAGE_TAG="${tag}" \
+                   -e YAML_UPDATE_FILE="${YAML_UPDATE_FILE}" \
+                   -e GIT_USER_EMAIL="${GIT_USER_EMAIL}" \
+                   -e GIT_USER_NAME="${GIT_USER_NAME}" \
+                   -e GIT_COMMIT_MESSAGE="${GIT_COMMIT_MESSAGE}" \
+                   -e PUSH_PARAMS="https://${GITHUB_TOKEN}@${K8S_REPO_SLUG} ${K8S_REPO_BRANCH}" \
                    orihoch/github_yaml_updater
         [ "$?" != "0" ] && echo failed github yaml update && exit 1
    fi
